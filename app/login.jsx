0a59940a5e445ac4fca601c74a9182f78bc01bf0
Mark Gardner <themarkgardner@gmail.com>
2025-05-28 11:08:39 -0500

    changed order of login button

--- app/login.jsx
+++ app/login.jsx
@@ -3,15 +3,16 @@ import React from 'react';
 export default function Login() {
   return (
     <>
-      <header className="App-header">
-        <button className="btn-spotify" onClick={() => window.location.href = "/api/auth/login"}>
-          Login with Spotify
-        </button>
+      <header >
+        
       </header>
       <main>
         <h1>Welcome to Illuminated</h1>
         <p>This project was made to experiment with CoPilot AI as a programming partner.</p>
         <p>If you're a PREMIUM user of Spotify, you can login and access your music through the player.</p>
+        <button className="btn-spotify" onClick={() => window.location.href = "/api/auth/login"}>
+          Login with Spotify
+        </button>
       </main>
     </>
   );
